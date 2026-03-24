55e3c0f0f013ac43540a0a17d2feec8c59649a2b
Myles Wirth <regime_skylark.0y@icloud.com>
2026-03-23 20:08:54 -0400

    add: connection type
    for managing the different types of connections in a structured way

--- asyncgit/src/graph/mod.rs
+++ asyncgit/src/graph/mod.rs
@@ -3,3 +3,22 @@ pub mod chunk;
 pub mod oids;
 pub mod walker;
 
+pub use walker::GraphWalker;
+
+#[derive(Clone, Debug, PartialEq)]
+pub enum ConnType {
+	Vertical,
+	VerticalDotted,
+	CommitNormal,
+	CommitBranch,
+	CommitMerge,
+	CommitStash,
+	CommitUncommitted,
+	MergeBridgeStart,
+	MergeBridgeMid,
+	MergeBridgeEnd,
+	BranchDown,
+	BranchUp,
+	BranchUpRight,
+}
+
